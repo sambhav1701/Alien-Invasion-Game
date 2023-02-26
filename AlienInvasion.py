0bd2dfe83de3f3cd9b0f7d5aefc97d459fcd6146
Sambhav Jain <sambhav.1701@outlook.com>
2023-02-26 14:57:08 -0600

    difficulty

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -53,6 +53,7 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            self.settings.initialize_dynamic_settings()
             pygame.mouse.set_visible(False) 
             self.stats.reset_stats()
             self.stats.game_active = True
@@ -94,6 +95,7 @@ class AlienInvasion:
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         self._check_fleet_edges()
