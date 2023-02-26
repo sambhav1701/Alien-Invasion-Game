3b6213a9bb51463bae2a9f8fe47f675422e260af
Sambhav Jain <sambhav.1701@outlook.com>
2023-02-25 21:39:41 -0600

    alien

--- class AlienInvasion.py
+++ class AlienInvasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
 
@@ -18,6 +19,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
         pygame.display.set_caption("Alien Invasion")
 
         
@@ -35,12 +38,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    self._check_keydown_events(event)
-            
-                    #if event.key == pygame.K_RIGHT:
-                    #    self.ship.moving_right = True
-                    #elif event.key == pygame.K_LEFT:
-                    #    self.ship.moving_left = True    
+                    self._check_keydown_events(event)  
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
 
@@ -71,12 +69,16 @@ class AlienInvasion:
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        alien = Alien(self)
+        self.aliens.add(alien)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
 
         pygame.display.flip()
 
