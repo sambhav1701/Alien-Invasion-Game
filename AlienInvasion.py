03076f78a8460e1284ea53c469d9860be99f9a9d
Sambhav Jain <sambhav.1701@outlook.com>
2023-02-25 22:30:19 -0600

    repopulate aliens

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -69,6 +69,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
                 if bullet.rect.bottom <= 0:
                     self.bullets.remove(bullet)
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         self._check_fleet_edges()
