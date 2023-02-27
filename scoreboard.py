eae29d4d59273105c24d9d383e3c6f97862e5ceb
Sambhav Jain <sambhav.1701@outlook.com>
2023-02-26 22:49:39 -0600

    bg black

--- scoreboard.py
+++ scoreboard.py
@@ -19,7 +19,7 @@ class Scoreboard:
     def prep_score(self):
         rounded_score = round(self.stats.score, -1)
         score_str = "Current Score: " + "{:,}".format(rounded_score)
-        self.score_image = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+        self.score_image = self.font.render(score_str, True, (255, 255, 255), self.settings.bg_color)
         self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
@@ -27,7 +27,7 @@ class Scoreboard:
     def prep_high_score(self):
         high_score = round(self.stats.high_score, -1)
         high_score_str = "Highest Score: " + "{:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True, self.text_color, self.settings.bg_color)
+        self.high_score_image = self.font.render(high_score_str, True, (255, 255, 255), self.settings.bg_color)
         self.high_score_rect = self.high_score_image.get_rect()
         self.high_score_rect.centerx = self.screen_rect.centerx
         self.high_score_rect.top = self.score_rect.top
@@ -46,7 +46,7 @@ class Scoreboard:
 
     def prep_level(self):
         level_str = "Level: " + str(self.stats.level)
-        self.level_image = self.font.render(level_str, True, self.text_color, self.settings.bg_color)
+        self.level_image = self.font.render(level_str, True, (255, 255, 255), self.settings.bg_color)
         self.level_rect = self.level_image.get_rect()
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10   
